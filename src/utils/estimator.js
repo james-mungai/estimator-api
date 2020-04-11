ee16653d30db43b5060faf5a08eaf791e6e38b70
james-mungai <jamesmungai.code@gmail.com>
2020-04-11 16:32:26 +0300

    added changes to estimator function

--- src/utils/estimator.js
+++ src/utils/estimator.js
@@ -1,27 +1,27 @@
 const covid19ImpactEstimator = (data) => {
     const {
-      reportedCases, totalHospitalBeds, region, timeToElapse, periodType
+      reportedCases, totalHospitalBeds, region, timeToElapse, periodType,
     } = data;
   
-    let normalisedTimeToElapse;
+    let timeToElapseInDays;
   
     switch (periodType) {
       case 'days':
-        normalisedTimeToElapse = timeToElapse;
+        timeToElapseInDays = timeToElapse;
         break;
       case 'weeks':
-        normalisedTimeToElapse = timeToElapse * 7;
+        timeToElapseInDays = timeToElapse * 7;
         break;
       case 'months':
-        normalisedTimeToElapse = timeToElapse * 30;
+        timeToElapseInDays = timeToElapse * 30;
         break;
       default:
         break;
     }
   
-    const { avgDailyIncomeInUSD } = region;
+    const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
   
-    const normalisedTimeFactor = 2**Math.floor(normalisedTimeToElapse / 3) ;
+    const normalisedTimeFactor = 2**Math.floor(timeToElapseInDays / 3) ;
   
     const currentlyInfectedNormal = reportedCases * 10;
     const currentlyInfectedSevere = reportedCases * 50;
@@ -30,15 +30,15 @@ const covid19ImpactEstimator = (data) => {
     const infectionsByRequestedTimeNormal = currentlyInfectedNormal * normalisedTimeFactor;
     const infectionsByRequestedTimeSevere = currentlyInfectedSevere * normalisedTimeFactor;
   
-    const severeCasesByRequestedTimeNormal = infectionsByRequestedTimeNormal * 0.15;
-    const severeCasesByRequestedTimeSevere = infectionsByRequestedTimeSevere * 0.15;
+    const severeCasesByRequestedTimeNormal = Math.floor(infectionsByRequestedTimeNormal * 0.15);
+    const severeCasesByRequestedTimeSevere = Math.floor(infectionsByRequestedSevere * 0.15);
   
-    const hsptlBedsByRqstdTimeNormal = Math.round(totalHospitalBeds * 0.35) - severeCasesByRequestedTimeNormal;
-    const hsptlBedsByRqstdTimeSevere = Math.round(totalHospitalBeds * 0.35) - severeCasesByRequestedTimeSevere;
+    const hsptlBedsByRqstdTimeNormal = Math.floor(totalHospitalBeds * 0.35) - severeCasesByRequestedTimeNormal;
+    const hsptlBedsByRqstdTimeSevere = Math.floor(totalHospitalBeds * 0.35) - severeCasesByRequestedTimeSevere;
     
   
-    const dollarsInFlightNormal = infectionsByRequestedTimeNormal * avgDailyIncomeInUSD * 30;
-    const dollarsInFlightSevere = infectionsByRequestedTimeSevere * avgDailyIncomeInUSD * 30;
+    const dollarsInFlightNormal = Math.floor(infectionsByRequestedTimeNormal * avgDailyIncomeInUSD * avgDailyIncomePopulation/30);
+    const dollarsInFlightSevere = Math.floor(infectionsByRequestedTimeSevere * avgDailyIncomeInUSD * avgDailyIncomePopulation/30);
     return {
       data,
       impact: {
@@ -46,8 +46,8 @@ const covid19ImpactEstimator = (data) => {
         infectionsByRequestedTime: infectionsByRequestedTimeNormal,
         severeCasesByRequestedTime: severeCasesByRequestedTimeNormal,
         hospitalBedsByRequestedTime: hsptlBedsByRqstdTimeNormal,
-        casesForICUByRequestedTime: infectionsByRequestedTimeNormal * 0.05,
-        casesForVentilatorsByRequestedTime: infectionsByRequestedTimeSevere * 0.02,
+        casesForICUByRequestedTime: Math.floor(infectionsByRequestedTimeNormal * 0.05),
+        casesForVentilatorsByRequestedTime: Math.floor(infectionsByRequestedTimeSevere * 0.02),
         dollarsInFlight: dollarsInFlightNormal
       }, // best case estimation
       severeImpact: {
@@ -55,27 +55,13 @@ const covid19ImpactEstimator = (data) => {
         infectionsByRequestedTime: infectionsByRequestedTimeSevere,
         severeCasesByRequestedTime: severeCasesByRequestedTimeSevere,
         hospitalBedsByRequestedTime: hsptlBedsByRqstdTimeSevere,
-        casesForICUByRequestedTime: infectionsByRequestedTimeSevere * 0.05,
-        casesForVentilatorsByRequestedTime: infectionsByRequestedTimeSevere * 0.02,
+        casesForICUByRequestedTime: Math.floor(infectionsByRequestedTimeSevere * 0.05),
+        casesForVentilatorsByRequestedTime: Math.floor(infectionsByRequestedTimeSevere * 0.02),
         dollarsInFlight: dollarsInFlightSevere
   
       } // severe case estimation
     };
   };
   
-  const data = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'weeks',
-    timeToElapse: 4,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  }
-
 
   module.exports = {covid19ImpactEstimator}
